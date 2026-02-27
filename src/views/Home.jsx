a94c574367ca0bc3aafe3019a4d2e17a62af8eea
Alonso Acosta <alonso.ae@hotmail.com>
2026-02-27 14:56:53 -0600

    Add tailwind css to the project

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-
 export default function Home() {
-    const [count, setCount] = useState(0)
-    
-    return <>
-        <h1>Scoresheet</h1>
-        <p>Count: {count}</p>
-        <button onClick={() => setCount(count + 1)}>Increment</button>
-    </>
+    return (
+        <div className='flex flex-col h-screen py-6 px-7'>
+            <h1 className='text-4xl py-1'>Games</h1>
+            Content
+        </div>
+    )
 }
